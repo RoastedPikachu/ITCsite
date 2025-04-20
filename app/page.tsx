6025dfbe2a4b711a4d31e48c11871f4b80605451
boris <b.karabyt@gmail.com>
2025-04-20 18:09:55 +0300

    feat: добавил раздел с документами

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import PageWrapper from "@/widgets/PageWrapper";
 
@@ -140,6 +140,118 @@ export default function Home() {
               <p className="text-white text-[2.8125rem] text-left font-['Montserrat'] font-semibold leading-[1.25]">
                 3D печать
               </p>
+            </div>
+          </div>
+        </section>
+
+        <section className="relative pt-[185px] px-[135px] w-full h-auto">
+          <h2 className="text-white text-[5.3125rem] text-left font-['Bebas'] font-bold leading-[1] uppercase">
+            <b className="text-[#00c9d7]">IT</b>-ПРОЕКТЫ: ОТ КОДА
+            <br /> ДО ВОПЛОЩЕНИЯ
+          </h2>
+
+          <div className="relative grid grid-cols-3 gap-x-[60px] mt-[90px] w-full">
+            <div className="px-[32px] p-[40px] w-full h-[26.5vw] rounded-[40px] bg-[url('/static/Development.png')] bg-cover">
+              <p className="text-white text-[2.8125rem] text-left font-['Montserrat'] font-semibold leading-[1.25]">
+                Проектная деятельность
+              </p>
+            </div>
+
+            <div className="px-[32px] p-[40px] w-full h-[26.5vw] rounded-[40px] bg-[url('/static/Media.png')] bg-cover">
+              <p className="text-white text-[2.8125rem] text-left font-['Montserrat'] font-semibold leading-[1.25]">
+                Медиа и менеджмент
+              </p>
+            </div>
+
+            <div className="px-[32px] p-[40px] w-full h-[26.5vw] rounded-[40px] bg-[url('/static/Development.png')] bg-cover">
+              <p className="text-white text-[2.8125rem] text-left font-['Montserrat'] font-semibold leading-[1.25]">
+                3D печать
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <section className="relative pt-[185px] px-[135px] w-full h-auto">
+          <h2 className="max-w-[650px] text-white text-[5.3125rem] font-['Bebas'] font-bold leading-[1] uppercase">
+            <b className="text-[#00c9d7]">С</b>ТАНЬ ЧАСТЬЮ НАШЕЙ <br />
+            IT-СЕМЬИ!
+          </h2>
+
+          <div className="grid grid-cols-2 gap-x-[85px] items-center mt-[90px] w-full h-auto">
+            <div className="relative w-full h-[27.5vw] bg-[url('/static/junarmy/Registration.png')] rounded-[35px]">
+              <div className="absolute left-0 bottom-0 w-full">
+                <p className="flex justify-center items-center ml-[30px] mb-[15px] w-[110px] h-[50px] bg-[#00c9d7] rounded-[10px] text-white text-[1.5rem] text-center font-['Montserrat'] font-bold uppercase">
+                  ШАГ 1
+                </p>
+
+                <p className="ml-[30px] mb-[30px] max-w-[355px] text-white text-[1.975rem] text-left font-['Montserrat'] font-semibold leading-[1.25]">
+                  Зарегистрируйтесь через форму
+                </p>
+
+                <div className="relative flex justify-center items-center w-full h-[194px] bg-[#0a0a0a] border-[1px] border-[#323232] rounded-[35px]">
+                  <a
+                    href=""
+                    className="py-[25px] w-[75%] h-auto bg-[#202020] rounded-[20px] text-white text-[1.5rem] text-center font-['Montserrat'] font-bold uppercase"
+                  >
+                    ЗАРЕГИСТРИРОВАТЬСЯ
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="relative w-full h-[27.5vw] bg-[url('/static/junarmy/Declaration.png')] rounded-[35px]">
+              <div className="absolute left-0 bottom-0 w-full">
+                <p className="flex justify-center items-center ml-[30px] mb-[15px] w-[110px] h-[50px] bg-[#00c9d7] rounded-[10px] text-white text-[1.5rem] text-center font-['Montserrat'] font-bold uppercase">
+                  ШАГ 2
+                </p>
+
+                <p className="ml-[30px] mb-[30px] max-w-[640px] text-white text-[1.975rem] text-left font-['Montserrat'] font-semibold leading-[1.25]">
+                  Заполните заявление и передайте его в 104 кабинет
+                </p>
+
+                <div className="relative flex justify-center items-center w-full h-[194px] bg-[#0a0a0a] border-[1px] border-[#323232] rounded-[35px]">
+                  <a
+                    href=""
+                    className="py-[25px] w-[75%] h-auto bg-[#202020] rounded-[20px] text-white text-[1.5rem] text-center font-['Montserrat'] font-bold uppercase"
+                  >
+                    СКАЧАТЬ ЗАЯВЛЕНИЕ
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="relative pt-[185px] pb-[135px] px-[135px] w-full h-auto">
+          <h2 className="max-w-[650px] text-white text-[5.3125rem] font-['Bebas'] font-bold leading-[1] uppercase">
+            <b className="text-[#00c9d7]">Д</b>ОКУМЕНТЫ ИТС
+          </h2>
+
+          <div className="grid gap-y-[60px] mt-[90px]">
+            <div className="flex justify-between items-center px-[50px] py-[50px] w-full h-auto border-[1px] border-[#323232] rounded-[30px]">
+              <p className="text-white text-[1.875rem] text-left font-['Montserrat'] font-semibold">
+                NDA (Соглашение о конфиденциальности и неразглашении информации)
+              </p>
+
+              <a
+                href="/static/docs/NDA.docx"
+                className="flex justify-center items-center w-[82px] h-[82px] bg-[#202020] rounded-[15px] outline-none"
+              >
+                <img src="/static/download.svg" alt="" />
+              </a>
+            </div>
+
+            <div className="flex justify-between items-center px-[50px] py-[50px] w-full h-auto border-[1px] border-[#323232] rounded-[30px]">
+              <p className="text-white text-[1.875rem] text-left font-['Montserrat'] font-semibold">
+                Заявление на вступление
+              </p>
+
+              <a
+                href="/static/docs/Zayavlenie-na-vstuplenie-v-ITS.docx"
+                className="flex justify-center items-center w-[82px] h-[82px] bg-[#202020] rounded-[15px] outline-none"
+              >
+                <img src="/static/download.svg" alt="" />
+              </a>
             </div>
           </div>
         </section>
