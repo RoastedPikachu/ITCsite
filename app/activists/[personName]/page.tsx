b3b7c89c81613aa1f027cf0bb73c07d16b430b09
boris <b.karabyt@gmail.com>
2025-04-25 13:56:30 +0300

    chore: Добавил функций для статической генерации страниц

--- app/activists/[personName]/page.tsx
+++ app/activists/[personName]/page.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
+import { activists } from "@/data/activists";
+
 import PageWrapper from "@/widgets/PageWrapper";
 import PersonPageContent from "@/widgets/pageContents/PersonPageContent";
 
-// export function generateStaticParams() {
-//   return activists.map((person: any) => ({
-//     personName: person.enName,
-//   }));
-// }
+export function generateStaticParams() {
+  return activists.map((person: any) => ({
+    personName: person.enName,
+  }));
+}
 
 const Page = () => {
   return (
--- app/management/[personName]/page.tsx
+++ app/management/[personName]/page.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
+import { management } from "@/data/management";
+
 import PageWrapper from "@/widgets/PageWrapper";
 import PersonPageContent from "@/widgets/pageContents/PersonPageContent";
 
-// export function generateStaticParams() {
-//   return management.map((person: any) => ({
-//     personName: person.enName,
-//   }));
-// }
+export function generateStaticParams() {
+  return management.map((person: any) => ({
+    personName: person.enName,
+  }));
+}
 
 const Page = () => {
   return (
